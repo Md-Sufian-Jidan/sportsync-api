34179c7e925055057e4a47996005ed478046e597
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2026-06-29 10:43:19 +0600

    added the extra environment variables for deploping in the render

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,17 +9,34 @@ import (
 
 type Config struct {
 	Port      string
-	Dsn       string
 	JwtSecret string
+
+	DatabaseURL string
+
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	DBSSLMode  string
 }
 
 func LoadEnv() *Config {
 	if err := godotenv.Load(); err != nil {
 		log.Println(".env file not found, using system environment variables")
 	}
+
 	return &Config{
 		Port:      os.Getenv("PORT"),
-		Dsn:       os.Getenv("DSN"),
 		JwtSecret: os.Getenv("JWT_SECRET"),
+
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBSSLMode:  os.Getenv("DB_SSL_MODE"),
 	}
 }
--- internal/config/db.go
+++ internal/config/db.go
@@ -1,12 +1,31 @@
 package config
 
 import (
+	"fmt"
+	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase(config *Config) *gorm.DB {
-	dsn := config.Dsn
+	var dsn string
+
+	if config.DatabaseURL != "" {
+		dsn = config.DatabaseURL
+		log.Println("Connecting to database using DATABASE_URL")
+	} else {
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC",
+			config.DBHost,
+			config.DBUser,
+			config.DBPassword,
+			config.DBName,
+			config.DBPort,
+			config.DBSSLMode,
+		)
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		TranslateError: true,
 	})
